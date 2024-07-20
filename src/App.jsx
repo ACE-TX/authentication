1f13d3029b1b6e7dccd0f582ef1cceeb76df18c5
Flarow189 <68514241+Flarow189@users.noreply.github.com>
2024-07-20 11:07:14 -0500

    Add files via upload
    Initial Code

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import SignUpForm from './SignUpForm';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from './Login';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      
+        <Route path='/signup' element={<SignUpForm />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+    
+    </Routes>
+    <SignUpForm />
+    </BrowserRouter>
+  );
+}
+
+export default App;
